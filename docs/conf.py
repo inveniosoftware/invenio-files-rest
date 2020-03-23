5cac9173a9e953b66414667f6838f26e830c6df7
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2020-03-23 11:19:49 +0100

    setup: bump invenio dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -314,7 +314,7 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'flask': ('http://flask.pocoo.org/docs/latest/', None),
+    'flask': ('https://flask.palletsprojects.com', None),
     'invenio-access': ('https://invenio-access.readthedocs.io/en/latest/',
                        None),
     'invenio-pidstore': ('https://invenio-pidstore.readthedocs.io/en/latest/',
