0bf2176e51c0ee08998ef00337f6264f00bbc334
Alex Dutton <alex@cottagelabs.com>
2020-08-19 10:59:22 +0100

    Add a class for computing checksums, counting bytes and reporting progress
    At the moment, the storage backend is responsible for computing checksums and
    reporting the size of files based on the number of bytes read. However, these
    are common concerns across all storage backends, and so this functionality can
    be pulled the other side of the storage backend interface.
    
    This class is therefore intended to wrap any file-like object, and to checksum
    the file as it is read. It also counts bytes and calls the progress callback if
    one is provided.
    
    In a subsequent commit we'll integrate it with FileInstance, and remove the
    functionality from the PyFS storage backend.

--- invenio_files_rest/utils.py
+++ invenio_files_rest/utils.py
@@ -7,8 +7,9 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 
 """Implementation of various utility functions."""
-
+import hashlib
 import mimetypes
+from typing import Callable
 
 import six
 from flask import current_app
@@ -56,3 +57,40 @@ def guess_mimetype(filename):
     if encoding:
         m = ENCODING_MIMETYPES.get(encoding, None)
     return m or 'application/octet-stream'
+
+
+class PassthroughChecksum:
+    def __init__(self, fp, hash_name, progress_callback: Callable[[int, int], None] = None, offset: int = 0):
+        """
+        :param fp: A file-like option open for reading
+        :param hash_name: A hashlib hash algorithm name
+        :param progress_callback: An optional function that will receive the number of bytes read, and the total file
+            size
+        """
+        self.hash_name = hash_name
+        self._sum = hashlib.new(hash_name)
+        self._fp = fp
+        self._bytes_read = 0
+        self._progress_callback = progress_callback
+        self._offset = offset
+
+    def read(self, size=-1):
+        chunk = self._fp.read(size)
+        self._bytes_read += len(chunk)
+        self._sum.update(chunk)
+        if self._progress_callback:
+            self._progress_callback(self._bytes_read, self._bytes_read + self._offset)
+        return chunk
+
+    def __getattr__(self, item):
+        return getattr(self._fp, item)
+
+    @property
+    def checksum(self):
+        """The {hash_name}:{hash} string for the bytes read so far."""
+        return '{0}:{1}'.format(
+            self.hash_name, self._sum.hexdigest())
+
+    @property
+    def bytes_read(self):
+        return self._bytes_read
