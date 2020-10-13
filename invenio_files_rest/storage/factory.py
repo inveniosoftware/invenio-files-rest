429504f5cfc8ec150f2b77c470c3e6e5d1e77ee0
Alex Dutton <alex@cottagelabs.com>
2020-10-13 20:56:46 +0100

    docstrings for the storage factory

--- invenio_files_rest/storage/factory.py
+++ invenio_files_rest/storage/factory.py
@@ -1,3 +1,5 @@
+"""Contains the base storage factory implementation."""
+
 import os
 import urllib.parse
 from typing import Any, Dict, Optional, Type
@@ -10,16 +12,24 @@ from ..helpers import make_path
 
 
 class StorageFactory:
-    """A base storage factory, with sensible default behaviour."""
+    """A base storage factory, with sensible default behaviour.
+
+    You may subclass this factory to implement custom behaviour. If you do this, remember to set
+    FILES_REST_STORAGE_FACTORY to the right import path for your subclass.
+    """
 
     def __init__(self, app):
+        """Initialize the storage factory."""
         self.app = app
 
     def __call__(
         self,
         fileinstance: FileInstance,
     ) -> Optional[StorageBackend]:
-        """Returns a FileStorage instance for a file, for manipulating file contents.
+        """Return a FileStorage instance for a file, for manipulating file contents.
+
+        This requires that the fileinstance already has an associated storage backend. If not,
+        you should call initialize() instead to initialize storage for the file instance.
         """
         if not fileinstance.storage_backend:
             return None
@@ -39,6 +49,11 @@ class StorageFactory:
         size: int = 0,
         preferred_location: Location = None
     ) -> StorageBackend:
+        """Initialize storage for a new file.
+
+        If provided, `preferred_location` will inform where the file will be stored, but may be ignored
+        if this factory is subclassed and `get_location()` is overridden.
+        """
         if fileinstance.storage_backend:
             return self(fileinstance)  # type: ignore
 
@@ -63,15 +78,19 @@ class StorageFactory:
         )
 
     def get_location(self, fileinstance: FileInstance, preferred_location: Location = None) -> Location:
+        """Return a Location for storing a new file.
+
+        This base implementation returns the preferred_location if it's provided, or the default location
+        recorded in the database. This method can be overridden if some other methodology is required.
+        """
         return preferred_location or Location.get_default()
 
-    def get_storage_backend_name(self, fileinstance: FileInstance, preferred_location: Location) -> str:
-        """"""
-        if not preferred_location:
-            raise ValueError("preferred_location required for default storage factory")
-        return preferred_location.storage_backend
-
     def resolve_storage_backend(self, backend_name: str) -> Type[StorageBackend]:
+        """Resolve a storage backend name to the associated storage backend class.
+
+        This base implementation resolves backends from the FILES_REST_STORAGE_BACKENDS app config
+        setting.
+        """
         return self.app.config['FILES_REST_STORAGE_BACKENDS'][backend_name]
 
     def get_storage_backend_kwargs(
@@ -79,6 +98,11 @@ class StorageFactory:
         fileinstance: FileInstance,
         storage_backend_cls: Type[StorageBackend],
     ) -> Dict[str, Any]:
+        """Retrieve any instantiation kwargs for the storage backend.
+
+        This returns an empty dict by defaut, but can be overridden to provide backend-specific
+        instantiation parameters if necessary.
+        """
         return {}
 
     def get_suggested_uri(
@@ -87,6 +111,12 @@ class StorageFactory:
         location: Location,
         storage_backend_cls: Type[StorageBackend],
     ):
+        """Generate a suggested URI for new files.
+
+        This can be overridden if your implementation requires some other file layout for storage.
+        Note that individual storage backends may choose to ignore the suggested URI if they
+        organise files by some other scheme.
+        """
         return make_path(
             location,
             str(fileinstance.id),
