fdc64b1df48fadb2b2fdd0bf6c9138ca9ca6d30e
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2021-12-09 13:18:03 +0100

    singals: add correct sender

--- invenio_files_rest/tasks.py
+++ invenio_files_rest/tasks.py
@@ -246,7 +246,7 @@ def merge_multipartobject(upload_id, version_id=None):
             progress_callback=progress_updater
         )
         db.session.commit()
-        file_uploaded.send(obj=obj)
+        file_uploaded.send(current_app._get_current_object(), obj=obj)
         return str(obj.version_id)
     except Exception:
         db.session.rollback()
