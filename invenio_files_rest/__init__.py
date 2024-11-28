6bffde8e02322ca129bede43eb3d0f241b717b7f
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-11-28 08:37:07 +0100

    release: v2.2.3

--- invenio_files_rest/__init__.py
+++ invenio_files_rest/__init__.py
@@ -973,7 +973,7 @@ a reference to the old :code:`FileInstance` to reference the new
 from .ext import InvenioFilesREST
 from .proxies import current_files_rest
 
-__version__ = "2.2.2"
+__version__ = "2.2.3"
 
 __all__ = (
     "__version__",
