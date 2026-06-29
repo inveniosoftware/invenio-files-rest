6c900c4da60b2c46359a237e88d98f6788a7e06b
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-29 16:21:33 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's a setuptools thing

--- setup.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2015-2022 CERN.
-# SPDX-License-Identifier: MIT
-
-"""Files download/upload REST API similar to S3 for Invenio."""
-
-from setuptools import setup
-
-setup()
